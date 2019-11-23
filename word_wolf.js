8cac5832d01b9177a37187554501bf08403d357d
Tsubasa Kato <tsubasa.kato.fub@gmail.com>
2019-11-23 22:26:37 +0900

    move test code

--- word_wolf.js
+++ word_wolf.js
@@ -2,8 +2,6 @@ window.onload = function(){
     sessionStorage.setItem('key', 'value');
 }
 
-// sessionStorage に保存したデータを取得する
-var data = sessionStorage.getItem('key');
 function test(){
     alert(sessionStorage.getItem('key') );
 }
