e38e0124bd948ac3682be22dc8a0977a27867879
Tsubasa Kato <tsubasa.kato.fub@gmail.com>
2019-12-04 16:16:22 +0900

    refactoring: result.js

--- result.js
+++ result.js
@@ -1,7 +1,7 @@
-let gameObject = JSON.parse(sessionStorage.getItem('current_wf_game'));
 document.addEventListener('DOMContentLoaded', initPage);
 
 function initPage(){
+    gameObject = JSON.parse(sessionStorage.getItem('current_wf_game'));
     document.getElementById("minor_word").textContent = gameObject.minorWord;
     document.getElementById("major_word").textContent = gameObject.majorWord;
 
