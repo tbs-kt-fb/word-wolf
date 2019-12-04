d8745b3024633f625ce9a014097b0ef86a5f6686
Tsubasa Kato <tsubasa.kato.fub@gmail.com>
2019-12-04 10:28:17 +0900

    refactor: playing.html playing.js

--- playing.js
+++ playing.js
@@ -2,30 +2,31 @@ let timerElement;
 let time;
 document.addEventListener('DOMContentLoaded', function () {
     timerElement = document.getElementById("timer-count");
-    time = 60 * 1000 * 3;
+    time = 60 * 3;
     displayTimer(time);
 });
 
-function updateTimer(millisecconds) {
-    if(millisecconds == null){
-        millisecconds = time;
-    }
-    let intervalId = setTimeout(function () {
-        millisecconds -= 1000;
-        if (millisecconds <= 0) {
-            clearInterval(intervalId);
-            millisecconds = 0;
+function startTimer(button){
+    button.setAttribute("disabled", "disabled");
+    updateTimer(time);
+}
+
+function updateTimer(secconds) {
+    setTimeout(function () {
+        secconds -= 1;
+        if (secconds <= 0) {
+            secconds = 0;
         }else{
-            updateTimer(millisecconds);
+            updateTimer(secconds);
         }
-        displayTimer(millisecconds);
+        displayTimer(secconds);
         
     }, 1000);
 }
 
 function displayTimer(millisecconds){
-    let minuite = ('00' + Math.floor(millisecconds / 60000)).slice(-2);
-    let second = ('00' + Math.floor((millisecconds % 60000) / 1000)).slice(-2);
+    let minuite = ('00' + Math.floor(millisecconds / 60)).slice(-2);
+    let second = ('00' + (millisecconds % 60)).slice(-2);
     if(millisecconds === 0){
         timerElement.textContent = "タイムアップ！"
     }else{
